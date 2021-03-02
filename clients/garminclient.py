27d437a41a6eabad3eafb31da499ddf86927afe0
dkmxbe <dkmxbe@noreply.github.com>
2021-03-02 12:52:31 +0100

    Internal garmin changes ..

--- clients/garminclient.py
+++ clients/garminclient.py
@@ -8,12 +8,17 @@ import sys
 import requests
 
 class GarminClient(object):
-    _SSO_LOGIN_URL = "https://sso.garmin.com/sso/signin"
-    _WORKOUT_SERVICE_URL = "https://connect.garmin.com/modern/proxy/workout-service"
-    _CALENDAR_SERVICE_URL = "https://connect.garmin.com/modern/proxy/calendar-service"
+
+    SSO_LOGIN_URL = "https://sso.garmin.com/sso/login"
+    """Garmin Connect's Single-Sign On login URL."""
+    SSO_SIGNIN_URL = "https://sso.garmin.com/sso/signin"
+    """The Garmin Connect Single-Sign On sign-in URL. This is where the login form
+    gets POSTed."""
+
+    _WORKOUT_SERVICE_URL = "https://connect.garmin.com/proxy/workout-service"
+    _CALENDAR_SERVICE_URL = "https://connect.garmin.com/proxy/calendar-service"
 
     _REQUIRED_HEADERS = {
-        "Referer": "https://connect.garmin.com/modern/workouts",
         "nk": "NT"
     }
 
@@ -40,6 +45,13 @@ class GarminClient(object):
                 "start": start_index,
                 "limit": batch_size
             }
+            """ Other param options
+                "myWorkoutsOnly": "true",
+                "sharedWorkoutsOnly": "true",
+                "orderBy": "WORKOUT_NAME",
+                "orderSeq": "ASC",
+                "includeAtp": "false"
+            """
             response = self.session.get(GarminClient._WORKOUT_SERVICE_URL + "/workouts", params=params)
             response.raise_for_status()
 
@@ -71,8 +83,7 @@ class GarminClient(object):
 
     def get_schedule(self, year, month):
         assert self.session
-        # https://connect.garmin.com/modern/proxy/calendar-service/year/2020/month/10/day/5/start/1
-        # response = self.session.get(GarminClient._CALENDAR_SERVICE_URL + "/year/%s/month/%s/day/%s/start/1" % (year, month - 1, day))
+        
         response = self.session.get(GarminClient._CALENDAR_SERVICE_URL + "/year/%s/month/%s" % (year, month - 1))
         response.raise_for_status()
 
@@ -136,21 +147,53 @@ class GarminClient(object):
         form_data = {
             "username": self.username,
             "password": self.password,
-            "embed": "false"
+            "embed": "false",
+            "_csrf": self._get_csrf_token(),
         }
-        request_params = {
-            "service": "https://connect.garmin.com/modern"
-        }
-        headers = {'origin': 'https://sso.garmin.com'}
 
+        headers = {'origin': 'https://sso.garmin.com'}
+    
         auth_response = self.session.post(
-            GarminClient._SSO_LOGIN_URL, headers=headers, params=request_params, data=form_data)
+            GarminClient.SSO_SIGNIN_URL, headers=headers, params=self._auth_params(), data=form_data)
         auth_response.raise_for_status()
 
+        self._LOG.debug("got auth response: %s", auth_response.text)
+        
         auth_ticket_url = self._extract_auth_ticket_url(auth_response.text)
 
+        self._LOG.debug("auth ticket url: '%s'", auth_ticket_url)
+
+        self._LOG.info("claiming auth ticket ...")
         response = self.session.get(auth_ticket_url)
         response.raise_for_status()
+
+        # appears like we need to touch base with the main page to complete the
+        # login ceremony.
+        self.session.get('https://connect.garmin.com/modern')
+
+    def _auth_params(self):
+        """A set of request query parameters that need to be present for Garmin to
+        accept our login attempt.
+        """
+        return {
+            "service": "https://connect.garmin.com/modern/",
+            "gauthHost": "https://sso.garmin.com/sso",
+        }
+
+    def _get_csrf_token(self):
+        """Retrieves a Cross-Site Request Forgery (CSRF) token from Garmin's login
+        page. The token is passed along in the login form for increased
+        security."""
+        self._LOG.info("fetching CSRF token ...")
+        resp = self.session.get(GarminClient.SSO_LOGIN_URL, params=self._auth_params())
+        if resp.status_code != 200:
+            raise ValueError("auth failure: could not load {}".format(GarminClient.SSO_LOGIN_URL))
+        # extract CSRF token
+        csrf_token = re.search(r'<input type="hidden" name="_csrf" value="(\w+)"',
+                               resp.content.decode('utf-8'))
+        if not csrf_token:
+            raise ValueError("auth failure: no CSRF token in {}".format(GarminClient.SSO_LOGIN_URL))
+        return csrf_token.group(1)
 
     @staticmethod
     def _extract_auth_ticket_url(auth_response):
